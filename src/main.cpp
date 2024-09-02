4eabbe8f2822445b69f46b15ebd4d1a30682f9b0
yang1206 <yang1206@outlook.com.tr>
2024-09-02 14:30:03 +0800

    format CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,11 @@
 #include <QApplication>
 #include <QLayout>
 #include <QPushButton>
-#include <fmt/core.h>
-#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[]) {
-    spdlog::info("Welcome to spdlog!");
-    spdlog::warn("This is a warning!");
-
-    // 格式化输出
-    spdlog::info("Hello, {}!", "world");
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
     button.show();
-    fmt::print("Hello, World!\n");
     return QApplication::exec();
 }
