6da1c0969ab6a44d4b4efff0c15ab2ab48d58e7a
yang1206 <y1149221897@outlook.com>
2024-11-04 15:09:40 +0800

    feat: 兼容windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <QApplication>
+#include <QScreen>
+#include "ElaApplication.h"
 #include "mainwindow.h"
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    int fontId = QFontDatabase::addApplicationFont(":/include/Font/ElaAwesome.ttf");
-    if (fontId == -1) {
-        qDebug() << "Failed to load font ElaAwesome.ttf";
-    } else {
-        qDebug() << "Successfully loaded font, id:" << fontId;
-        qDebug() << "Font families:" << QFontDatabase::applicationFontFamilies(fontId);
-    }
+    eApp->init();
     MainWindow w;
     w.show();
     return a.exec();
