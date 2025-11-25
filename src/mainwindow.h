5ff8091279067db5bea76efc865690040c088ac6
yang1206 <yang15188280673@outlook.com>
2025-11-25 16:15:48 +0800

    chore: 移除qwindowkit

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,7 +4,6 @@
 #include <QMainWindow>
 #include <QPushButton>
 #include <QCustomPlot.h>
-#include "shared/widgetframe/windowframemanager.h"
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -21,15 +20,12 @@ class MainWindow : public QMainWindow {
   private:
     void initializeUI();
     void setupConnections();
-    void setupMenus();
-    void setupWindowEffects();
 
   private:
     QCustomPlot*      m_plot;
     QCustomPlot*      m_fftPlot;
     QPushButton*      m_button;
     QPushButton*      m_fftButton;
-    WindowFrameManager* m_frameManager;
     QTimer*           m_updateTimer;
     QVector<double>   m_timeData;
     QVector<double>   x;
